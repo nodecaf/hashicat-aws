3819e2a2fffb1422c4097971ef318b3e47422903
John Fjeldberg <John.Fjeldberg@gmail.com>
2022-08-18 04:36:48 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/megacorp/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = "${prefix}"
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
 }
